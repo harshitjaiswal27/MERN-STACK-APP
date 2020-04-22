a457041f76f6c59bb2b24d9163dfda20b05ff9ba
Harshit Jaiswal <jaiswalhk99@gmail.com>
2020-04-22 22:06:06 +0530

    Client done

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(cors());
 app.use(express.json());
 
 mongoose.connect('mongodb://localhost/MERN_STACK_APP', { useNewUrlParser: true, useCreateIndex: true });
