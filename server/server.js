48a85c17d2265b033f7c00dd628524bf6723cff9
Harshit Jaiswal <jaiswalhk99@gmail.com>
2020-04-22 21:21:15 +0530

    DB connection added & Models added

--- server/server.js
+++ server/server.js
@@ -1,8 +1,24 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(express.json());
+
+mongoose.connect('mongodb://localhost/MERN_STACK_APP', { useNewUrlParser: true, useCreateIndex: true });
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+      console.log("MongoDB database connection established successfully");
+})
+    
+const exercisesRouter = require('./routes/exercises');
+const usersRouter = require('./routes/users');
+    
+app.use('/exercises', exercisesRouter);
+app.use('/users', usersRouter);
+    
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
 });
